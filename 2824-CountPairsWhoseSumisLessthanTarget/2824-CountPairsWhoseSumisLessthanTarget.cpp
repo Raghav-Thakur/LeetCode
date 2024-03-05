d27e585110ece678315b3429d07b1a59743ed0a1
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-05 23:07:11 +0530

    Create file

---
+++ 2824-CountPairsWhoseSumisLessthanTarget/2824-CountPairsWhoseSumisLessthanTarget.cpp
@@ -0,0 +1,17 @@
+        int ans = 0,diff = 0;
+                diff = nums[i] + nums[j];
+                if (diff < target) {
+    int countPairs(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                }
+                    ans++;
+            }
+        }
+        return ans;
+class Solution {
+public:
+    }
+};
+
+[
