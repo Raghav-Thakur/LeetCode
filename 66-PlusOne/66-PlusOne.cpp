c6bd755d82bd52a54866687d24d94cafc3aab4a8
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-05 15:07:01 +0530

    Create file

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,12 @@
+
+      digits[i] = 0;
+    }
+      }
+        return digits;
+      if (digits[i] < 9) {
+        ++digits[i];
+    digits.insert(digits.begin(), 1);
+    return digits;
+  }
+};
+[
