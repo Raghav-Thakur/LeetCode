d1c2d37e2ac2670f80fc833cd4a7f2e1c2d1edd1
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-05 22:24:40 +0530

    Create file

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,19 @@
+                    ans.push_back(i);
+                    ans.push_back(j);
+        vector<int> ans;
+                if (nums[i] + nums[j] == target) {
+            for (int j = i + 1; j < nums.size(); j++) {
+        for (int i = 0; i < nums.size(); i++) {
+                    return ans;
+                }
+            }
+        }
+        return ans;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+#include <vector>
+
+using namespace std;
+[
