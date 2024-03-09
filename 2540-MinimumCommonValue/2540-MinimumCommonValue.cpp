53d235abb59d2a19f0edc54a96446c123b97427d
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-09 16:28:19 +0530

    Create file

---
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.cpp
@@ -0,0 +1,18 @@
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    
+            if (index != -1) {
+                return nums1[i];
+    int getCommon(const vector<int>& nums1, const vector<int>& nums2) {
+        for (int i = 0; i < nums1.size(); ++i) {
+            int index = BinarySearch(nums2, nums1[i], nums2.size());
+            }
+        }
+        return -1;
+    }
+};
+
+[
