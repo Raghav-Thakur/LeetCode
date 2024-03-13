f04f1702e227ac195f861eeec5bbb7e3eb3e4a6b
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-13 12:09:53 +0530

    Create file

---
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -0,0 +1,20 @@
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            }
+            return -1;
+        }
+            for(int i = 0; i <= size-1; i++) {
+                if (nums[i] == target) {
+                    return i;
+                }
+        int mid = 0;
+        int ans;
+        if (size < 1000) {
+    
+    int search(vector<int>& nums, int target) {
+        int size = nums.size();
+        int s = 0, e = size - 1;
+        }
+        return -1;
+    }
+[
