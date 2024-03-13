c2a8747d5e6f4af27df35869e26e8ce75b8942d2
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2024-03-13 09:33:31 +0530

    Create file

---
+++ 704-BinarySearch/704-BinarySearch.cpp
@@ -0,0 +1,18 @@
+        while (s <= e) {
+            mid = s + (e-s)/2;
+            if (nums[mid]==target) {
+        int mid = 0;
+        int s= 0, e= nums.size() -1;
+            if (nums[0] == target) {
+        }
+                return 0;
+            }
+            else {
+                return -1;
+            }
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                e = mid - 1;
+            }
+[
